d0059188eb67d19659eff275a1241515d5f2b206
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-12-03 16:05:55 +0100

    Solve second part

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,6 @@
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+
 fn parse_input(input: &str) -> Vec<String> {
     input
         .lines()
@@ -6,51 +9,63 @@ fn parse_input(input: &str) -> Vec<String> {
         .collect::<Vec<_>>()
 }
 
-/// Find the highest jolt, pair of combined digits.
-fn calculate_jolt(bank: &str) -> u32 {
-    let digits = bank
-        .chars()
-        .filter_map(|digit| digit.to_digit(10))
-        .collect::<Vec<_>>();
+fn calculate_jolt(bank: &str, num_batteries: usize) -> u64 {
+    let digits = bank.chars().collect::<Vec<_>>();
     let size = digits.len();
 
-    let mut first = digits[0];
-    let mut second = digits[1];
+    // keep list of indices
+    let mut indices: Vec<usize> = (0..num_batteries).into_iter().collect();
 
-    for i in 1..(size - 1) {
-        // find highest first digit
-        if digits[i] > first {
-            first = digits[i];
-            second = digits[i + 1];
-        }
+    // for each battery slot, determine the highest battery from available range
+    for pos in 0..num_batteries {
+        let mut battery = digits[indices[pos]];
+        let end = size - num_batteries + pos;
 
-        // find highest second digit in remaining range
-        for j in i + 1..size {
-            if digits[j] > second {
-                second = digits[j];
+        for index in (indices[pos] + 1)..=end {
+            let new_battery = digits[index];
+            if new_battery > battery {
+                battery = new_battery;
+                for (i, pos) in (pos..num_batteries).enumerate() {
+                    indices[pos] = index + i;
+                }
             }
         }
     }
 
-    first * 10 + second
+    indices
+        .into_iter()
+        .map(|index| digits[index])
+        .collect::<String>()
+        .parse::<u64>()
+        .expect("Failed to produce jolt")
+}
+
+fn process(banks: &[String], batteries: usize) -> u64 {
+    banks.iter().fold(0, |total, bank| {
+        total + calculate_jolt(bank.as_str(), batteries)
+    })
 }
 
 /// For each bank / string, get all digits and find the two highest ones
-fn process_part1(banks: &[String]) -> u32 {
-    banks
-        .iter()
-        .fold(0, |total, bank| total + calculate_jolt(bank.as_str()))
+fn process_part1(banks: &[String]) -> u64 {
+    process(banks, 2)
+}
+
+fn process_part2(banks: &[String]) -> u64 {
+    process(banks, 12)
 }
 
 fn main() {
     let input = parse_input(include_str!("input.txt"));
     let result = process_part1(&input);
     println!("PART 1: {}", result);
+    let result = process_part2(&input);
+    println!("PART 2: {}", result);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{calculate_jolt, parse_input, process_part1};
+    use crate::{calculate_jolt, parse_input, process_part1, process_part2};
 
     const INPUT: &str = r#"
         987654321111111
@@ -66,13 +81,23 @@ mod tests {
     }
 
     #[test]
+    fn check_part2() {
+        let input = parse_input(INPUT);
+        assert_eq!(3121910778619, process_part2(&input));
+    }
+
+    #[test]
     fn test_calculate_jolt() {
-        assert_eq!(89, calculate_jolt("811111111111119"));
-        assert_eq!(
-            99,
-            calculate_jolt(
-                "2215452689925244273244333436189317446384838478525478824435233342352236255624326767355438753493222423"
-            )
-        );
+        assert_eq!(89, calculate_jolt("811111111111119", 2));
+        assert_eq!(888911112111, calculate_jolt("818181911112111", 12));
+        assert_eq!(987654321111, calculate_jolt("987654321111111", 12));
+        assert_eq!(811111111119, calculate_jolt("811111111111119", 12));
+        assert_eq!(434234234278, calculate_jolt("234234234234278", 12));
+    }
+
+    #[test]
+    fn test_calculate_extra_jolt() {
+        assert_eq!(891, calculate_jolt("8111191", 3));
+        assert_eq!(8191, calculate_jolt("8111191", 4));
     }
 }
