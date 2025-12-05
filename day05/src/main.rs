b4609e16384999210044a0d735bc07a2e6502b45
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-12-05 11:35:05 +0100

    Add 2nd solution for day 5

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -17,27 +17,39 @@ fn parse_input(input: &str) -> (Vec<(Id, Id)>, Vec<Id>) {
     (ranges, ingredients)
 }
 
-fn process_part1((ranges, ingredients): (Vec<(Id, Id)>, Vec<Id>)) -> usize {
+fn process_part1((ranges, ingredients): &(Vec<(Id, Id)>, Vec<Id>)) -> usize {
     ingredients
-        .into_iter()
+        .iter()
         .filter(|id| {
             ranges
                 .iter()
-                .find(|(min, max)| min <= id && id <= max)
+                .find(|(min, max)| min <= id && max >= id)
                 .is_some()
         })
         .count()
 }
 
+/// NOTE: ranges can overlap.
+fn process_part2((ranges, _): &(Vec<(Id, Id)>, Vec<Id>)) -> usize {
+    // keep each range, compare with existing ranges, merge existing ranges when they overlap
+    let mut result: Vec<(Id, Id)> = Vec::new();
+
+    for range in ranges {
+
+    }
+
+    result.iter().map(|(min, max)| max - min).sum::<u64>() as usize
+}
+
 fn main() {
     let input = parse_input(include_str!("input.txt"));
-    let result = process_part1(input);
+    let result = process_part1(&input);
     println!("PART 1: {}", result);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_input, process_part1};
+    use crate::*;
 
     const INPUT: &str = r#"3-5
 10-14
@@ -60,6 +72,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(3, process_part1(parse_input(INPUT)));
+        assert_eq!(3, process_part1(&parse_input(INPUT)));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(14, process_part2(&parse_input(INPUT)));
     }
 }
