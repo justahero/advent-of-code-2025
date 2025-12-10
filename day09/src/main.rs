ed77314d43572700d773b297b7e95592f2134300
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-12-10 21:35:11 +0100

    Implement second half of day 9

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,12 +1,14 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::fmt::Display;
+
 use itertools::Itertools;
 
 #[derive(Debug, Clone, Copy)]
 struct Pos {
-    x: i64,
-    y: i64,
+    pub x: i64,
+    pub y: i64,
 }
 
 impl Pos {
@@ -16,6 +18,12 @@ impl Pos {
 
     pub fn area(&self, rhs: &Pos) -> i64 {
         ((self.x - rhs.x).abs() + 1) * ((self.y - rhs.y).abs() + 1)
+    }
+}
+
+impl Display for Pos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
     }
 }
 
@@ -35,8 +43,17 @@ impl Edge {
     }
 
     /// Returns true if the position is considered "inside"
+    ///
+    /// See https://www.geeksforgeeks.org/dsa/direction-point-line-segment/
     pub fn inside(&self, pos: &Pos) -> bool {
-        true
+        println!(":: inside: {},{} - pos: {}", self.start, self.end, pos);
+
+        let Self { start, end } = self;
+        if start.x == pos.x || start.y == pos.y || end.x == pos.x || end.y == pos.y {
+            return true;
+        }
+
+        (end.x - start.x) * (pos.y - start.y) - (end.y - start.y) * (pos.x - start.x) > 0
     }
 }
 
@@ -62,18 +79,22 @@ fn process_part1(positions: &[Pos]) -> u64 {
 /// Returns the set of edges spanning an area. Positions are red tiles, all other tiles of the area
 /// are green tiles.
 fn get_edges(positions: &[Pos]) -> Vec<Edge> {
-    let mut edges = positions
+    positions
         .iter()
-        .tuple_windows()
+        .circular_tuple_windows()
         .map(|(l, r)| Edge::new(*l, *r))
-        .collect::<Vec<_>>();
-    edges.push(Edge::new(positions[positions.len() - 1], positions[0]));
-    edges
+        .collect::<Vec<_>>()
 }
 
 /// Checks if the pair of positions is completely inside the area outlined by edges
-fn inside_area(edges: &[Edge], left: &Pos, right: &Pos) -> bool {
-    false
+fn inside_polygon(edges: &[Edge], l: &Pos, r: &Pos) -> bool {
+    edges.iter().all(|Edge { start, end }| {
+        let left = l.x.max(r.x) <= start.x.min(end.x);
+        let right = l.x.min(r.x) >= start.x.max(end.x);
+        let above = l.y.max(r.y) <= start.y.min(end.y);
+        let below = l.y.min(r.y) >= start.y.max(end.y);
+        left || right || above || below
+    })
 }
 
 /// Given is the list of red tiles.
@@ -82,10 +103,15 @@ fn inside_area(edges: &[Edge], left: &Pos, right: &Pos) -> bool {
 fn process_part2(positions: &[Pos]) -> u64 {
     let edges = get_edges(positions);
 
-    let mut max_area: u64 = 0;
-    for (l, r) in positions.iter().tuple_combinations() {}
-
-    max_area
+    let (_, _, area) = positions
+        .iter()
+        .tuple_combinations::<(_, _)>()
+        .map(|(l, r)| (l, r, l.area(r)))
+        .sorted_by_key(|item| item.2)
+        .rev()
+        .find(|(l, r, area)| inside_polygon(&edges, l, r))
+        .expect("Failed to find area");
+    area as u64
 }
 
 fn main() {
@@ -98,7 +124,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Pos, get_edges, inside_area, parse_input, process_part1, process_part2};
+    use crate::{Edge, Pos, get_edges, inside_polygon, parse_input, process_part1, process_part2};
 
     const INPUT: &str = r#"
         7,1
@@ -135,20 +161,27 @@ mod tests {
     }
 
     #[test]
+    fn test_edge_inside() {
+        let edge = Edge::new(Pos::new(7, 1), Pos::new(11, 1));
+        assert!(edge.inside(&Pos::new(8, 2)));
+        assert!(!edge.inside(&Pos::new(8, 0)));
+    }
+
+    #[test]
     fn test_inside_area() {
         let positions = parse_input(INPUT);
         let edges = get_edges(&positions);
-        assert!(inside_area(
+        assert!(inside_polygon(
             &edges,
             &Pos { x: 7, y: 3 },
             &Pos { x: 11, y: 1 }
         ));
-        assert!(inside_area(
+        assert!(inside_polygon(
             &edges,
             &Pos { x: 9, y: 7 },
             &Pos { x: 9, y: 5 }
         ));
-        assert!(inside_area(
+        assert!(inside_polygon(
             &edges,
             &Pos { x: 9, y: 5 },
             &Pos { x: 2, y: 3 }
