8b89e7df85d8c5884c6e7d48f5f02d269f13af61
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-12-11 11:10:06 +0100

    Start to implement algorithm to determine fewest presses
    * refactor types, use `BitVec` struct

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::ops::Shl;
+
 use nom::{
     IResult, Parser,
     branch::alt,
@@ -9,23 +11,66 @@ use nom::{
     sequence::delimited,
 };
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct BitVec(u16);
+
+impl BitVec {
+    pub fn new(bits: &[u8]) -> Self {
+        let bits = bits.iter().fold(0u16, |mut bits, index| {
+            debug_assert!(*index < 16);
+            bits |= 1u16.shl(index);
+            bits
+        });
+        Self(bits)
+    }
+
+    pub fn bits(&self) -> u16 {
+        self.0
+    }
+
+    pub fn toggle(&self, rhs: &BitVec) -> Self {
+        Self(self.0 ^ rhs.0)
+    }
+}
+
 struct Machine {
     // [.##.]
-    lights: Vec<u8>,
+    lights: BitVec,
     // (3) (1,3) (2) (2,3) (0,2) (0,1)
-    buttons: Vec<Vec<u16>>,
+    buttons: Vec<BitVec>,
     // joltage requirements
-    joltage: Vec<u16>,
+    joltage: Vec<u8>,
 }
 
 impl Machine {
-    pub fn new(lights: Vec<u8>, buttons: Vec<Vec<u16>>, joltage: Vec<u16>) -> Self {
+    pub fn new(lights: Vec<u8>, buttons: Vec<BitVec>, joltage: Vec<u8>) -> Self {
         Self {
-            lights,
+            lights: BitVec::new(&lights),
             buttons,
             joltage,
         }
     }
+
+    /// Determine the number of fewest presses to match the indicator lights, e.g. `[.##.]`.
+    ///
+    /// Initially all lights are off, a sequence with the fewest pressed buttons is calculated.
+    pub fn fewest_presses(&self) -> u32 {
+        0
+    }
+}
+
+///
+fn recursive(target: &BitVec, lights: BitVec, buttons: &[BitVec], level: u32) -> bool {
+    if target == &lights {
+        return true;
+    }
+
+    for button in buttons.iter() {
+        if recursive(target, lights.toggle(button), buttons, level + 1) {
+            return true;
+        }
+    }
+    false
 }
 
 fn parse_input(input: &str) -> Vec<Machine> {
@@ -48,6 +93,7 @@ fn parse_machine(line: &str) -> Machine {
         .parse(line)
         .expect("Failed to parse input");
 
+    let buttons = buttons.iter().map(|b| BitVec::new(&b)).collect::<Vec<_>>();
     Machine::new(lights, buttons, joltage)
 }
 
@@ -70,47 +116,60 @@ fn parse_lights(input: &str) -> IResult<&str, Vec<u8>> {
     .parse(input)
 }
 
-fn parse_buttons_list(input: &str) -> IResult<&str, Vec<Vec<u16>>> {
+fn parse_buttons_list(input: &str) -> IResult<&str, Vec<Vec<u8>>> {
     separated_list1(space1, parse_buttons).parse(input)
 }
 
-fn parse_buttons(input: &str) -> IResult<&str, Vec<u16>> {
+fn parse_buttons(input: &str) -> IResult<&str, Vec<u8>> {
     delimited(
         tag("("),
-        separated_list1(tag(","), nom::character::complete::u16),
+        separated_list1(tag(","), nom::character::complete::u8),
         tag(")"),
     )
     .parse(input)
 }
 
-fn parse_joltage(input: &str) -> IResult<&str, Vec<u16>> {
+fn parse_joltage(input: &str) -> IResult<&str, Vec<u8>> {
     delimited(
         tag("{"),
-        separated_list1(tag(","), nom::character::complete::u16),
+        separated_list1(tag(","), nom::character::complete::u8),
         tag("}"),
     )
     .parse(input)
 }
 
-fn process_part1(machines: &[Machine]) -> u64 {
-    0
+fn process_part1(machines: &[Machine]) -> u32 {
+    machines.iter().map(Machine::fewest_presses).sum::<u32>()
 }
 
 fn main() {
     let machines = parse_input(include_str!("input.txt"));
     let result = process_part1(&machines);
+    println!("PART 1: {}", result);
     // TODO
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_buttons_list, parse_input, parse_joltage, parse_lights};
+    use crate::{BitVec, parse_buttons_list, parse_input, parse_joltage, parse_lights};
 
     const INPUT: &str = r#"
 [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
     "#;
+
+    #[test]
+    fn test_buttons_constructor() {
+        assert_eq!(0b1001, BitVec::new(&[0, 3]).0);
+    }
+
+    #[test]
+    fn test_buttons_toggle() {
+        let expected = BitVec::new(&[0, 3]);
+        let lights = BitVec::new(&[1, 2]);
+        assert_eq!(expected, lights.toggle(&BitVec::new(&[0, 1, 2, 3])));
+    }
 
     #[test]
     fn test_parse_lights() {
